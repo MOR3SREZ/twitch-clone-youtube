1b23b0a523437b97a516172192284e3b8c0d24ec
moretza rezaeian <morteza78rezaeian@gmail.com>
2022-06-19 12:07:03 +0430

    init project

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className='fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10'>
+      {/* Left Side */}
+      <div className='flex grow items-center justify-start'></div>
+
+      {/* Middle */}
+
+      {/* Right Side  */}
+    </div>
+  );
+};
+export default Navbar;
